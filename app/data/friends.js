15855b489a9ccecb50dc19a5f101c5717aca5122
Joshua Moore <jommoore2003@me.com>
2019-01-01 17:22:47 -0800

    Adding friend finder assignment files

---
+++ app/data/friends.js
@@ -0,0 +1,101 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'Darth Vader',
+		photo: 'https://boygeniusreport.files.wordpress.com/2015/08/darth-vader.jpg?quality=98&strip=all&w=500',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Larry The Cable Guy',
+		photo: 'https://dehayf5mhw1h7.cloudfront.net/wp-content/uploads/sites/601/2016/05/04141054/Larry-the-cable-guy-that-film-guy-2utscjos.jpg',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Ellen DeGeneres',
+		photo: 'https://assets-news-bcdn.dailyhunt.in/cmd/resize/400x400_60/fetchdata13/images/fc/e5/1d/fce51ddf50713f8c36faa4322c58b018.jpg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Macaulay Culkin',
+		photo: 'https://images.hellogiggles.com/uploads/2017/06/23073021/macaulayculkin5.jpg',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Prince Harry',
+		photo: 'https://assets.smoothradio.com/2016/38/prince-harry-1474348835-view-1.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Kermit the Frog',
+		photo: 'https://www.wnd.com/files/2011/11/kermit-frog-muppets.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+];
+
+module.exports = friendsArray;
